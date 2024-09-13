036ffa8f157c851373c92a3fe3d27ecda80722cf
tanay-683 <tanay.r.sharma@gmail.com>
2024-09-13 16:26:57 +0530

    structure created

---
+++ template.py
@@ -0,0 +1,41 @@
+import os, logging
+from pathlib import Path
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s :: %(message)s")
+project_name = "cnnClassifier"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/config/__init__.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/constant/__init__.py",
+
+    f"{project_name}/config/configuration.py",
+    "config/config.yaml",  # ye folder upar wale se alag h
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+]
+
+
+for file_path in list_of_files:
+    file_path = Path(file_path) # convert to Path object toh handle forward and backward slash problem
+    file_dir, file_name = os.path.split(file_path)
+    
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok=True)
+        logging.info(f"Created directory: {file_dir} for the file: {file_name}")
+    
+    if (not os.path.exists(file_path)) or os.path.getsize(file_path) == 0:
+        with open(file_path, "w") as f:
+            f.write("")
+        logging.info(f"Empty File Created: {file_name}")
+    else:
+        logging.info(f"File already exists: {file_name}")
